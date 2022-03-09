9ba894f106d5892e9c306ff429e6acf4dd681ceb
AmenoAtsuta <zuiho-taberyu@outlook.jp>
2022-03-10 00:01:18 +0900

    キャラクターシート倉庫との非同期通信でJsonpの取得テスト完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import fetchJsonp from "fetch-jsonp"
 import logo from './logo.svg';
 import './App.css';
 
 const App:React.VFC=()=>{
   const [system,setSystem]=useState<string>("stellar")
-  const [sheetId,setSheetId]=useState<string>("")
-  useEffect(()=>{})
+  const [sheetId,setSheetId]=useState<string>("ahVzfmNoYXJhY3Rlci1zaGVldHMtbXByFwsSDUNoYXJhY3RlckRhdGEY4onv8QMM")
+  const [resCharaSheet,setResCharaSheet]=useState({});
+
+  useEffect(()=>{
+    fetchJsonp(`http://character-sheets.appspot.com/${system}/display?ajax=1&key=${sheetId}`,{
+      jsonpCallback: 'callback',
+    })
+    .then((res)=>{return res.json()})
+    .then((json)=>{setResCharaSheet(json)})
+    .then(()=>{console.log(resCharaSheet)})
+    .catch(err=>console.log(err))
+
+  },[])
 
   return (
     <div className="App">
