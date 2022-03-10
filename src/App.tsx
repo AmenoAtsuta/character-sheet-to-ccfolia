ccbe45415a6a6f9cc7ee550e2cbe5bbf41d2f76d
AmenoAtsuta <zuiho-taberyu@outlook.jp>
2022-03-10 21:23:50 +0900

    説明書き等を追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,50 @@
 import React, { useEffect, useState } from 'react';
 import fetchJsonp from "fetch-jsonp"
 import {Button, TextField} from '@material-ui/core';
+import { css } from '@emotion/react'
 import { styled } from '@mui/material/styles'
-import { createTheme } from "@material-ui/core/styles";
-import { ThemeProvider } from "@emotion/react";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {indigo, red} from '@mui/material/colors'
 import {ToStellarKnightsCcfolia} from "./to_charasheet_json/StellarKnights"
-import logo from './logo.svg';
 import './App.css';
-import { alertTitleClasses } from '@mui/material';
+
+const darkTheme=createTheme({
+  palette:{
+    mode: 'dark',
+  },
+  typography:{
+    fontFamily:[
+      "Noto Sans JP",
+      "Lato"
+    ].join(",")
+  }
+});
 
 const App:React.VFC=()=>{
   const [system,setSystem]=useState<string>("stellar")
   const [sheetId,setSheetId]=useState<string>("")
   const [resCharaSheet,setResCharaSheet]=useState({});
+
+  const inputForms=css`
+    margin: 2.5% 0;
+  `
+  const inputId=css`
+    width:40%;
+  `
+
+  const howToUseUl=css`
+    width:70%;
+    margin:0 auto;
+  `
+
+  const howToUseLi=css`
+    list-style-type:demical;
+    margin:2% 0;
+  `
+
+  const spanEmphasis=css`
+    color:red;
+  `
 
   useEffect(()=>{
     if(sheetId!=="" && system!==""){
@@ -29,15 +61,17 @@ const App:React.VFC=()=>{
             break
         }
       })
-      .catch(err=>console.log(err))
+      .catch((err)=>{
+        console.log(err)
+      })
   }
   },[sheetId,system])
 
-  const IdInputForm:React.VFC=()=>{
+  const InputForm:React.VFC=()=>{
     return (
-      <div>
+      <div css={inputForms}>
         <form>
-          <TextField variant="standard" onChange={idHandleChange} value={sheetId} />
+          <TextField variant="standard" label="ここにIDをペースト" onChange={idHandleChange} value={sheetId} css={inputId} />
         </form>
       </div>
     )
@@ -58,12 +92,29 @@ const App:React.VFC=()=>{
 
   
   return (
-    <div className="App">
-      <IdInputForm/>
-      <Button variant="contained" onClick={()=>copyCharaSheetJson()}>
-        ココフォリア出力用にコピー
-      </Button>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <h1>ココフォリア用コマ作成ツール</h1>
+        <h2>概要</h2>
+        <p>
+          キャラクターシート倉庫からキャラシを取得して、ココフォリアへとペーストしコマを作成できるツールです。<br/>
+          現在アルファ版として銀剣のステラナイツにのみ対応しています。<br/>
+          将来的にはキャラクターシート保管庫への対応、及び他システムへの対応(アリアンロッド2Eやダブルクロス3rd辺りが優先になりそう。CoCは優秀な先駆者様がいるので対応予定は現状ありません。)<br/>
+          ココフォリアだけでなく、ユドナリウムへの出力にも対応したいと思っています。
+        </p>
+        <h2>使い方</h2>
+        <ul css={howToUseUl}>
+          <li css={howToUseLi}>キャラクターシート倉庫のキャラシページからIDをコピーする(URLのhttps://character-sheets.appspot.com/stellar/edit.html?key=<span css={spanEmphasis}>[ここに書いてある長い英数字の羅列がID]</span>)</li>
+          <li css={howToUseLi}>"ここにIDをペースト"と書いてあるところにコピーしたキャラシIDをペースト</li>
+          <li css={howToUseLi}>"ココフォリア出力用にコピー"ボタンをクリックして、"クリップボードにコピーしました"と表示されれば成功</li>
+          <li css={howToUseLi}>ココフォリアにペーストするとコマが出来上がります</li>
+        </ul>
+        <InputForm/>
+        <Button variant="contained" onClick={()=>copyCharaSheetJson()}>
+          ココフォリア出力用にコピー
+        </Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
